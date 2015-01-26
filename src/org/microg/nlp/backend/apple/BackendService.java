df2e29d39a96e5022ef22c240208f8df09bc16c7
mar-v-in <github@rvin.mooo.com>
2015-01-26 02:18:02 +0100

    Fix whitespacing, use WiFiBackendHelper, remove unnacassary receiver

--- src/org/microg/nlp/backend/apple/BackendService.java
+++ src/org/microg/nlp/backend/apple/BackendService.java
@@ -1,161 +1,149 @@
 package org.microg.nlp.backend.apple;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.location.Location;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+
 import org.microg.nlp.api.LocationBackendService;
 import org.microg.nlp.api.LocationHelper;
+import org.microg.nlp.api.WiFiBackendHelper;
 
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-public class BackendService extends LocationBackendService {
+public class BackendService extends LocationBackendService implements WiFiBackendHelper.Listener {
 
-	private static final String TAG = BackendService.class.getName();
-	private static final long THIRTY_DAYS = 2592000000L;
-	private final BroadcastReceiver wifiBroadcastReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			if (running) reportUpdate();
-		}
-	};
-	private final LocationRetriever retriever = new LocationRetriever();
-	private boolean running = false;
-	private VerifyingWifiLocationCalculator calculator;
-	private WifiLocationDatabase database;
-	private WifiManager wifiManager;
-	private Thread thread;
-	private Set<String> toRetrieve;
-	private final Runnable retrieveAction = new Runnable() {
-		@Override
-		public void run() {
-			while (toRetrieve != null && !toRetrieve.isEmpty()) {
-				if (running) {
-					Set<String> now = new HashSet<String>();
-					for (String s : toRetrieve) {
-						now.add(s);
-						if (now.size() == 10) break;
-					}
-					Log.d(TAG, "Requesting Apple for " + now.size() + " locations");
-					try {
-						Collection<Location> response = retriever.retrieveLocations(now);
-						WifiLocationDatabase.Editor editor = database.edit();
-						for (Location location : response) {
-							editor.put(location);
-							toRetrieve.remove(location.getExtras().getString(LocationRetriever.EXTRA_MAC_ADDRESS));
-						}
-						for (String mac : now) {
-							if (toRetrieve.contains(mac)) {
-								Bundle extras = new Bundle();
-								extras.putString(LocationRetriever.EXTRA_MAC_ADDRESS, mac);
-								editor.put(LocationHelper.create("unknown", System.currentTimeMillis(), extras));
-								toRetrieve.remove(mac);
-							}
-						}
-						editor.end();
-						// Forcing update, because new mapping data is available
-						reportUpdate();
-					} catch (Exception e) {
-						Log.w(TAG, e);
-					}
-				}
-				synchronized (thread) {
-					try {
-						thread.wait(30000);
-					} catch (InterruptedException e) {
-						break;
-					}
-				}
-			}
-			toRetrieve = null;
-			thread = null;
-		}
-	};
+    private static final String TAG = BackendService.class.getName();
+    private static final long THIRTY_DAYS = 2592000000L;
+    private final LocationRetriever retriever = new LocationRetriever();
+    private boolean running = false;
+    private WiFiBackendHelper backendHelper;
+    private VerifyingWifiLocationCalculator calculator;
+    private WifiLocationDatabase database;
+    private Thread thread;
+    private Set<String> toRetrieve;
+    private final Runnable retrieveAction = new Runnable() {
+        @Override
+        public void run() {
+            while (toRetrieve != null && !toRetrieve.isEmpty()) {
+                if (running) {
+                    Set<String> now = new HashSet<>();
+                    for (String s : toRetrieve) {
+                        now.add(s);
+                        if (now.size() == 10) break;
+                    }
+                    Log.d(TAG, "Requesting Apple for " + now.size() + " locations");
+                    try {
+                        Collection<Location> response = retriever.retrieveLocations(now);
+                        WifiLocationDatabase.Editor editor = database.edit();
+                        for (Location location : response) {
+                            editor.put(location);
+                            toRetrieve.remove(location.getExtras().getString(LocationRetriever
+                                    .EXTRA_MAC_ADDRESS));
+                        }
+                        for (String mac : now) {
+                            if (toRetrieve.contains(mac)) {
+                                Bundle extras = new Bundle();
+                                extras.putString(LocationRetriever.EXTRA_MAC_ADDRESS, mac);
+                                editor.put(LocationHelper.create("unknown",
+                                        System.currentTimeMillis(), extras));
+                                toRetrieve.remove(mac);
+                            }
+                        }
+                        editor.end();
+                        // Forcing update, because new mapping data is available
+                        report(calculate());
+                    } catch (Exception e) {
+                        Log.w(TAG, e);
+                    }
+                }
+                synchronized (thread) {
+                    try {
+                        thread.wait(30000);
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+            }
+            toRetrieve = null;
+            thread = null;
+        }
+    };
 
-	@Override
-	protected Location update() {
-		if (wifiManager != null) {
-			if (wifiManager.isWifiEnabled() || Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2 && wifiManager.isScanAlwaysAvailable()) {
-				wifiManager.startScan();
-			}
-		}
-		return null;
-	}
+    @Override
+    protected Location update() {
+        backendHelper.onUpdate();
+        return null;
+    }
 
-	private synchronized Location calculate() {
-		if (!running) {
-			return null;
-		}
-		Collection<ScanResult> scanResults = wifiManager.getScanResults();
-		Set<Location> locations = new HashSet<Location>();
-		Set<String> unknown = new HashSet<String>();
-		for (ScanResult result : scanResults) {
-			if (result.SSID.endsWith("_nomap")) {
-				// It is industry standard to ignore those APs, so we'll do the same
-				continue;
-			}
-			String mac = LocationRetriever.wellFormedMac(result.BSSID);
-			Location location = database.get(mac);
-			if (location != null) {
-				if ((location.getTime() + THIRTY_DAYS) < System.currentTimeMillis()) {
-					// Location is old, let's refresh it :)
-					unknown.add(mac);
-				}
-				location.getExtras().putInt(LocationRetriever.EXTRA_SIGNAL_LEVEL, result.level);
-				if (location.hasAccuracy() && location.getAccuracy() != -1) {
-					locations.add(location);
-				}
-			} else {
-				unknown.add(mac);
-			}
-		}
-		Log.d(TAG, "Found " + scanResults.size() + " wifis, of whom " + locations.size() + " with location and " + unknown.size() + " unknown.");
-		if (!unknown.isEmpty()) {
-			if (toRetrieve == null) {
-				toRetrieve = unknown;
-			} else {
-				toRetrieve.addAll(unknown);
-			}
-		}
-		if (thread == null) {
-			thread = new Thread(retrieveAction);
-			thread.start();
-		}
-		return calculator.calculate(locations);
-	}
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        backendHelper = new WiFiBackendHelper(this, this);
+    }
 
-	private void reportUpdate() {
-		report(calculate());
-	}
+    private synchronized Location calculate() {
+        if (!running) {
+            return null;
+        }
+        Set<WiFiBackendHelper.WiFi> wiFis = backendHelper.getWiFis();
+        Set<Location> locations = new HashSet<>();
+        Set<String> unknown = new HashSet<>();
+        for (WiFiBackendHelper.WiFi wifi : wiFis) {
+            Location location = database.get(wifi.getBssid());
+            if (location != null) {
+                if ((location.getTime() + THIRTY_DAYS) < System.currentTimeMillis()) {
+                    // Location is old, let's refresh it :)
+                    unknown.add(wifi.getBssid());
+                }
+                location.getExtras().putInt(LocationRetriever.EXTRA_SIGNAL_LEVEL, wifi.getRssi());
+                if (location.hasAccuracy() && location.getAccuracy() != -1) {
+                    locations.add(location);
+                }
+            } else {
+                unknown.add(wifi.getBssid());
+            }
+        }
+        Log.d(TAG, "Found " + wiFis.size() + " wifis, of whom " + locations.size() + " with " +
+                "location and " + unknown.size() + " unknown.");
+        if (!unknown.isEmpty()) {
+            if (toRetrieve == null) {
+                toRetrieve = unknown;
+            } else {
+                toRetrieve.addAll(unknown);
+            }
+        }
+        if (thread == null) {
+            thread = new Thread(retrieveAction);
+            thread.start();
+        }
+        return calculator.calculate(locations);
+    }
 
-	@Override
-	protected synchronized void onOpen() {
-		database = new WifiLocationDatabase(this);
-		calculator = new VerifyingWifiLocationCalculator("apple", database);
-		wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
-		registerReceiver(wifiBroadcastReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-		running = true;
-	}
+    @Override
+    protected synchronized void onOpen() {
+        database = new WifiLocationDatabase(this);
+        calculator = new VerifyingWifiLocationCalculator("apple", database);
+        backendHelper.onOpen();
+        running = true;
+    }
 
-	@Override
-	protected synchronized void onClose() {
-		running = false;
-		unregisterReceiver(wifiBroadcastReceiver);
-		calculator = null;
-		database.close();
-		if (thread != null) {
-			thread.interrupt();
-			thread = null;
-		}
-		database = null;
-		wifiManager = null;
-	}
+    @Override
+    protected synchronized void onClose() {
+        running = false;
+        backendHelper.onClose();
+        calculator = null;
+        database.close();
+        if (thread != null) {
+            thread.interrupt();
+            thread = null;
+        }
+        database = null;
+    }
+
+    @Override
+    public void onWiFisChanged(Set<WiFiBackendHelper.WiFi> wiFis) {
+        if (running) report(calculate());
+    }
 }
--- src/org/microg/nlp/backend/apple/VerifyingWifiLocationCalculator.java
+++ src/org/microg/nlp/backend/apple/VerifyingWifiLocationCalculator.java
@@ -3,171 +3,191 @@ package org.microg.nlp.backend.apple;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+
 import org.microg.nlp.api.LocationHelper;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class VerifyingWifiLocationCalculator {
-	private static final String TAG = VerifyingWifiLocationCalculator.class.getName();
+    private static final String TAG = VerifyingWifiLocationCalculator.class.getName();
 
-	private static final long ONE_DAY = 24 * 60 * 60 * 1000;
-	private static final int MAX_WIFI_RADIUS = 500;
-	private static final float ACCURACY_WEIGHT = 50;
-	private static final int MIN_SIGNAL_LEVEL = -200;
-	private final WifiLocationDatabase database;
-	private final String provider;
+    private static final long ONE_DAY = 24 * 60 * 60 * 1000;
+    private static final int MAX_WIFI_RADIUS = 500;
+    private static final float ACCURACY_WEIGHT = 50;
+    private static final int MIN_SIGNAL_LEVEL = -200;
+    private final WifiLocationDatabase database;
+    private final String provider;
 
-	public VerifyingWifiLocationCalculator(String provider, WifiLocationDatabase database) {
-		this.database = database;
-		this.provider = provider;
-	}
+    public VerifyingWifiLocationCalculator(String provider, WifiLocationDatabase database) {
+        this.database = database;
+        this.provider = provider;
+    }
 
-	private static Set<Set<Location>> divideInClasses(Set<Location> locations, double accuracy) {
-		Set<Set<Location>> classes = new HashSet<Set<Location>>();
-		for (Location location : locations) {
-			boolean used = false;
-			for (Set<Location> locClass : classes) {
-				if (locationCompatibleWithClass(location, locClass, accuracy)) {
-					locClass.add(location);
-					used = true;
-				}
-			}
-			if (!used) {
-				Set<Location> locClass = new HashSet<Location>();
-				locClass.add(location);
-				classes.add(locClass);
-			}
-		}
-		return classes;
-	}
+    private static Set<Set<Location>> divideInClasses(Set<Location> locations, double accuracy) {
+        Set<Set<Location>> classes = new HashSet<Set<Location>>();
+        for (Location location : locations) {
+            boolean used = false;
+            for (Set<Location> locClass : classes) {
+                if (locationCompatibleWithClass(location, locClass, accuracy)) {
+                    locClass.add(location);
+                    used = true;
+                }
+            }
+            if (!used) {
+                Set<Location> locClass = new HashSet<Location>();
+                locClass.add(location);
+                classes.add(locClass);
+            }
+        }
+        return classes;
+    }
 
-	private static boolean locationCompatibleWithClass(Location location, Set<Location> locClass, double accuracy) {
-		for (Location other : locClass) {
-			if ((location.distanceTo(other) - location.getAccuracy() - other.getAccuracy() -
-					accuracy) < 0) {
-				return true;
-			}
-		}
-		return false;
-	}
+    private static boolean locationCompatibleWithClass(Location location, Set<Location> locClass,
+            double accuracy) {
+        for (Location other : locClass) {
+            if ((location.distanceTo(other) - location.getAccuracy() - other.getAccuracy() -
+                    accuracy) < 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	private static void combineClasses(Set<Set<Location>> classes, double accuracy) {
-		/*
-		 * TODO
-		 * The old routine was never tested and caused Exceptions in the rare case it has to do something
-		 * Need to write a new one here, but first priority is to not cause problems
-		 */
-	}
+    private static void combineClasses(Set<Set<Location>> classes, double accuracy) {
+        /*
+         * TODO
+         * The old routine was never tested and caused Exceptions in the rare case it has to do
+         * something
+         * Need to write a new one here, but first priority is to not cause problems
+          */
+    }
 
-	public Location calculate(Set<Location> locations) {
-		Set<Set<Location>> locationClasses = divideInClasses(locations, MAX_WIFI_RADIUS);
-		combineClasses(locationClasses, MAX_WIFI_RADIUS);
-		List<Set<Location>> clsList = new ArrayList<Set<Location>>(locationClasses);
-		Collections.sort(clsList, new Comparator<Set<Location>>() {
-			@Override
-			public int compare(Set<Location> lhs, Set<Location> rhs) {
-				return rhs.size() - lhs.size();
-			}
-		});
-		StringBuilder sb = new StringBuilder("Build classes of size:");
-		for (Set<Location> set : clsList) {
-			sb.append(" ").append(set.size());
-		}
-		Log.d(TAG, sb.toString());
-		if (!clsList.isEmpty()) {
-			Set<Location> cls = clsList.get(0);
-			if (cls.size() == 1) {
-				Location location = cls.iterator().next();
-				if (isVerified(location)) {
-					Log.d(TAG, "is single class, but verified.");
-					return location;
-				}
-				return null;
-			} else if (cls.size() == 2) {
-				boolean verified = false;
-				for (Location location : cls) {
-					if (isVerified(location)) {
-						verified = true;
-						break;
-					}
-				}
-				if (verified) {
-					Log.d(TAG, "is dual class and verified.");
-					verify(cls);
-				} else {
-					Log.d(TAG, "is dual class, but not verified.");
-				}
-			} else if (cls.size() > 2) {
-				Log.d(TAG, "is multi class and auto-verified.");
-				verify(cls);
-			}
-			return combine(cls);
-		}
-		return null;
-	}
+    public Location calculate(Set<Location> locations) {
+        Set<Set<Location>> locationClasses = divideInClasses(locations, MAX_WIFI_RADIUS);
+        combineClasses(locationClasses, MAX_WIFI_RADIUS);
+        List<Set<Location>> clsList = new ArrayList<Set<Location>>(locationClasses);
+        Collections.sort(clsList, new Comparator<Set<Location>>() {
+            @Override
+            public int compare(Set<Location> lhs, Set<Location> rhs) {
+                return rhs.size() - lhs.size();
+            }
+        });
+        StringBuilder sb = new StringBuilder("Build classes of size:");
+        for (Set<Location> set : clsList) {
+            sb.append(" ").append(set.size());
+        }
+        Log.d(TAG, sb.toString());
+        if (!clsList.isEmpty()) {
+            Set<Location> cls = clsList.get(0);
+            if (cls.size() == 1) {
+                Location location = cls.iterator().next();
+                if (isVerified(location)) {
+                    Log.d(TAG, "is single class, but verified.");
+                    return location;
+                }
+                return null;
+            } else if (cls.size() == 2) {
+                boolean verified = false;
+                for (Location location : cls) {
+                    if (isVerified(location)) {
+                        verified = true;
+                        break;
+                    }
+                }
+                if (verified) {
+                    Log.d(TAG, "is dual class and verified.");
+                    verify(cls);
+                } else {
+                    Log.d(TAG, "is dual class, but not verified.");
+                }
+            } else if (cls.size() > 2) {
+                Log.d(TAG, "is multi class and auto-verified.");
+                verify(cls);
+            }
+            return combine(cls);
+        }
+        return null;
+    }
 
-	private int getSignalLevel(Location location) {
-		return Math.abs(location.getExtras().getInt(LocationRetriever.EXTRA_SIGNAL_LEVEL) - MIN_SIGNAL_LEVEL);
-	}
+    private int getSignalLevel(Location location) {
+        return Math.abs(location.getExtras().getInt(LocationRetriever.EXTRA_SIGNAL_LEVEL) -
+                MIN_SIGNAL_LEVEL);
+    }
 
-	private Location combine(Set<Location> locations) {
-		float minSignal = Integer.MAX_VALUE, maxSignal = Integer.MIN_VALUE;
-		for (Location location : locations) {
-			minSignal = Math.min(minSignal, getSignalLevel(location));
-			maxSignal = Math.max(maxSignal, getSignalLevel(location));
-		}
-		double totalWeight = 0;
-		double latitude = 0;
-		double longitude = 0;
-		float accuracy = 0;
-		double altitudeWeight = 0;
-		double altitude = 0;
-		long verified = -1;
-		for (Location location : locations) {
-			if (location != null) {
-				double weight = Math.pow((((float) (getSignalLevel(location)) - minSignal) / (maxSignal - minSignal))
-						+ ACCURACY_WEIGHT / Math.max(location.getAccuracy(), ACCURACY_WEIGHT), 2);
-				Log.d(TAG, String.format("Using with weight=%f mac=%s signal=%d accuracy=%f latitude=%f longitude=%f", weight, location.getExtras().getString(LocationRetriever.EXTRA_MAC_ADDRESS), location.getExtras().getInt(LocationRetriever.EXTRA_SIGNAL_LEVEL), location.getAccuracy(), location.getLatitude(), location.getLongitude()));
-				totalWeight += weight;
-				latitude += location.getLatitude() * weight;
-				longitude += location.getLongitude() * weight;
-				accuracy += location.getAccuracy() * weight;
-				if (location.hasAltitude()) {
-					altitude += location.getAltitude() * weight;
-					altitudeWeight += weight;
-				}
-				if (location.getExtras().containsKey(LocationRetriever.EXTRA_VERIFIED_TIME)) {
-					verified = Math.max(verified, location.getExtras().getLong(LocationRetriever.EXTRA_VERIFIED_TIME));
-				}
-			}
-		}
-		if (locations.size() < 3) {
-			// This location can't be accurate
-			accuracy *= 2;
-		}
-		Bundle extras = new Bundle();
-		extras.putInt("COMBINED_OF", locations.size());
-		if (verified != -1) {
-			extras.putLong(LocationRetriever.EXTRA_VERIFIED_TIME, verified);
-		}
-		if (altitudeWeight > 0) {
-			return LocationHelper.create(provider, latitude / totalWeight, longitude / totalWeight, altitude / altitudeWeight, (float) (accuracy / totalWeight), extras);
-		} else {
-			return LocationHelper.create(provider, latitude / totalWeight, longitude / totalWeight, (float) (accuracy / totalWeight), extras);
-		}
-	}
+    private Location combine(Set<Location> locations) {
+        float minSignal = Integer.MAX_VALUE, maxSignal = Integer.MIN_VALUE;
+        for (Location location : locations) {
+            minSignal = Math.min(minSignal, getSignalLevel(location));
+            maxSignal = Math.max(maxSignal, getSignalLevel(location));
+        }
+        double totalWeight = 0;
+        double latitude = 0;
+        double longitude = 0;
+        float accuracy = 0;
+        double altitudeWeight = 0;
+        double altitude = 0;
+        long verified = -1;
+        for (Location location : locations) {
+            if (location != null) {
+                double weight = Math.pow((((float) (getSignalLevel(location)) - minSignal) /
+                        (maxSignal - minSignal))
+                        + ACCURACY_WEIGHT / Math.max(location.getAccuracy(), ACCURACY_WEIGHT), 2);
+                Log.d(TAG, String.format("Using with weight=%f mac=%s signal=%d accuracy=%f " +
+                                "latitude=%f longitude=%f", weight, location.getExtras().getString
+                                (LocationRetriever.EXTRA_MAC_ADDRESS), location.getExtras().getInt
+                                (LocationRetriever.EXTRA_SIGNAL_LEVEL), location.getAccuracy(),
+                        location.getLatitude(), location.getLongitude()));
+                totalWeight += weight;
+                latitude += location.getLatitude() * weight;
+                longitude += location.getLongitude() * weight;
+                accuracy += location.getAccuracy() * weight;
+                if (location.hasAltitude()) {
+                    altitude += location.getAltitude() * weight;
+                    altitudeWeight += weight;
+                }
+                if (location.getExtras().containsKey(LocationRetriever.EXTRA_VERIFIED_TIME)) {
+                    verified = Math.max(verified, location.getExtras().getLong(LocationRetriever
+                            .EXTRA_VERIFIED_TIME));
+                }
+            }
+        }
+        if (locations.size() < 3) {
+            // This location can't be accurate
+            accuracy *= 2;
+        }
+        Bundle extras = new Bundle();
+        extras.putInt("COMBINED_OF", locations.size());
+        if (verified != -1) {
+            extras.putLong(LocationRetriever.EXTRA_VERIFIED_TIME, verified);
+        }
+        if (altitudeWeight > 0) {
+            return LocationHelper.create(provider, latitude / totalWeight,
+                    longitude / totalWeight, altitude / altitudeWeight,
+                    (float) (accuracy / totalWeight), extras);
+        } else {
+            return LocationHelper.create(provider, latitude / totalWeight,
+                    longitude / totalWeight, (float) (accuracy / totalWeight), extras);
+        }
+    }
 
-	private void verify(Set<Location> cls) {
-		WifiLocationDatabase.Editor editor = database.edit();
-		for (Location location : cls) {
-			location.getExtras().putLong(LocationRetriever.EXTRA_VERIFIED_TIME, System.currentTimeMillis());
-			editor.put(location);
-		}
-		editor.end();
-	}
+    private void verify(Set<Location> cls) {
+        WifiLocationDatabase.Editor editor = database.edit();
+        for (Location location : cls) {
+            location.getExtras().putLong(LocationRetriever.EXTRA_VERIFIED_TIME,
+                    System.currentTimeMillis());
+            editor.put(location);
+        }
+        editor.end();
+    }
 
-	private boolean isVerified(Location location) {
-		return location.getExtras().getLong(LocationRetriever.EXTRA_VERIFIED_TIME) > System.currentTimeMillis() - ONE_DAY;
-	}
+    private boolean isVerified(Location location) {
+        return location.getExtras().getLong(LocationRetriever.EXTRA_VERIFIED_TIME) > System
+                .currentTimeMillis() - ONE_DAY;
+    }
 
 }
